4504fae478bd43ad6bb316d11e50484f0cb0577a
vkp3012 <vivekpatel301295@gmail.com>
2023-02-01 00:39:51 +0530

    all error is done

--- src/components/contect/form.js
+++ src/components/contect/form.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import emailjs from "@emailjs/browser";
-import {service_id,temp,public_key}from '../../secrects'
+import {service_id,temp,public_key}from '../../secrets'
 import './form.css';
 
 const Form = () => {
